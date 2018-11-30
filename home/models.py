34f97d91030cd6320a7f8765d25b3a50bde0f399
dpham55 <43793444+dpham55@users.noreply.github.com>
2018-11-29 22:58:00 -0500

    Big Update
    tons of improvements

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 
 
 class HomePage(Page):
@@ -11,64 +11,78 @@ class HomePage(Page):
     partners_list = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-    	FieldPanel('about',classname="full"),
-    	FieldPanel('partners_blurb',classname="full"),
-    	FieldPanel('partners_list',classname="full"),
+    	FieldPanel('about',classname='full'),
+    	FieldPanel('partners_blurb',classname='full'),
+    	FieldPanel('partners_list',classname='full'),
     ]
 
 
 class AboutPage(Page):
     profile = RichTextField(blank=True)
-    government_clients = RichTextField(blank=True)
-    dod_clients = RichTextField(blank=True)
-    commercial_clients = RichTextField(blank=True)
     locations_blurb = RichTextField(blank=True)
-    hq_name = RichTextField(blank=True)
-    hq_details = RichTextField(blank=True)
-    second_office_name = RichTextField(blank=True)
-    second_office_details = RichTextField(blank=True)
-    third_office_name = RichTextField(blank=True)
-    third_office_details = RichTextField(blank=True)
-    award_one_name = RichTextField(blank=True)
-    award_one_details = RichTextField(blank=True)
-    award_two_name = RichTextField(blank=True)
-    award_two_details = RichTextField(blank=True)
-    award_three_name = RichTextField(blank=True)
-    award_three_details = RichTextField(blank=True)
-    award_four_name = RichTextField(blank=True)
-    award_four_details = RichTextField(blank=True)
-    award_five_name = RichTextField(blank=True)
-    award_five_details = RichTextField(blank=True)
-    award_six_name = RichTextField(blank=True)
-    award_six_details = RichTextField(blank=True)
-    history = RichTextField(blank=True)                                                             
+    history = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-    	FieldPanel('profile',classname="full"),
-    	FieldPanel('government_clients',classname="full"),
-    	FieldPanel('dod_clients',classname="full"),
-    	FieldPanel('commercial_clients',classname="full"),
-        FieldPanel('locations_blurb',classname="full"),
-    	FieldPanel('hq_name',classname="full"),
-    	FieldPanel('hq_details',classname="full"),
-    	FieldPanel('second_office_name',classname="full"),
-    	FieldPanel('second_office_details',classname="full"),
-    	FieldPanel('third_office_name',classname="full"),
-    	FieldPanel('third_office_details',classname="full"),
-    	FieldPanel('award_one_name',classname="full"),
-    	FieldPanel('award_one_details',classname="full"),
-    	FieldPanel('award_two_name',classname="full"),
-    	FieldPanel('award_two_details',classname="full"),
-    	FieldPanel('award_three_name',classname="full"),
-    	FieldPanel('award_three_details',classname="full"),
-    	FieldPanel('award_four_name',classname="full"),
-    	FieldPanel('award_four_details',classname="full"),
-    	FieldPanel('award_five_name',classname="full"),
-    	FieldPanel('award_five_details',classname="full"),
-    	FieldPanel('award_six_name',classname="full"),
-    	FieldPanel('award_six_details',classname="full"),
-    	FieldPanel('history',classname="full"),
+    	FieldPanel('profile',classname='full'),
+    	InlinePanel('government_clients',label='Government Clients'),
+    	InlinePanel('dod_clients',label='DoD Clients'),
+    	InlinePanel('commercial_clients',label='Commercial Clients'),
+        FieldPanel('locations_blurb',classname='full'),
+    	InlinePanel('office_locations',label='Offices'),
+    	InlinePanel('awards',label='Awards'),
+    	FieldPanel('history',classname='full'),
     ]
+
+class AboutPageAwards(Orderable):
+    page = ParentalKey(AboutPage, on_delete=models.CASCADE, related_name='awards')
+    award_name = models.CharField(blank=True, max_length=250)
+    award_description = models.CharField(blank=True, max_length=280)
+
+    panels = [
+        FieldPanel('award_name'),
+        FieldPanel('award_description'),
+    ]
+
+
+class AboutPageOffice(Orderable):
+    page = ParentalKey(AboutPage, on_delete=models.CASCADE, related_name='office_locations')
+    office_name = models.CharField(blank=True, max_length=250)
+    office_address = RichTextField(blank=True)
+    office_phone = models.CharField(blank=True, max_length=16)
+    office_fax = models.CharField(blank=True, max_length=16)
+
+    panels = [
+        FieldPanel('office_name'),
+        FieldPanel('office_address'),
+        FieldPanel('office_phone'),
+        FieldPanel('office_fax'),
+    ]
+
+
+class AboutPageGovernmentClients(Orderable):
+    page = ParentalKey(AboutPage, on_delete=models.CASCADE, related_name='government_clients')
+    client_name = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('client_name'),
+    ]
+
+
+class AboutPageDoDClients(Orderable):
+    page = ParentalKey(AboutPage, on_delete=models.CASCADE, related_name='dod_clients')
+    client_name = models.CharField(blank=True, max_length=250)
+    
+    panels = [
+        FieldPanel('client_name'),
+    ]
+
+class AboutPageCommercialClients(Orderable):
+    page = ParentalKey(AboutPage, on_delete=models.CASCADE, related_name='commercial_clients')
+    client_name = models.CharField(blank=True, max_length=250)
+    
+    panels = [
+        FieldPanel('client_name'),
+    ]    
 
 
 class ServicesPage(Page):
@@ -90,22 +104,22 @@ class ServicesPage(Page):
     technical_admin_pastperf = RichTextField(blank=True)
                             
     content_panels = Page.content_panels + [
-    	FieldPanel('approach',classname="full"),
-    	FieldPanel('personnel_blurb',classname="full"),
-    	FieldPanel('processes_blurb',classname="full"),
-    	FieldPanel('performance_blurb',classname="full"),
-    	FieldPanel('contract_vehicles_blurb',classname="full"),
-    	FieldPanel('contract_vehicles_list',classname="full"),
-    	FieldPanel('core_capabilities_list',classname="full"),
-    	FieldPanel('test_management_pastperf',classname="full"),
-    	FieldPanel('modeling_simulation_pastperf',classname="full"),
-    	FieldPanel('intel_pastperf',classname="full"),
-    	FieldPanel('cybersecurity_pastperf',classname="full"),
-    	FieldPanel('systems_analysis_eng_pastperf',classname="full"),
-    	FieldPanel('assistive_tech_pastperf',classname="full"),
-    	FieldPanel('document_management_pastperf',classname="full"),
-    	FieldPanel('distance_learning_pastperf',classname="full"),
-    	FieldPanel('technical_admin_pastperf',classname="full"),    	    	    	    	
+    	FieldPanel('approach',classname='full'),
+    	FieldPanel('personnel_blurb',classname='full'),
+    	FieldPanel('processes_blurb',classname='full'),
+    	FieldPanel('performance_blurb',classname='full'),
+    	FieldPanel('contract_vehicles_blurb',classname='full'),
+    	FieldPanel('contract_vehicles_list',classname='full'),
+    	FieldPanel('core_capabilities_list',classname='full'),
+    	FieldPanel('test_management_pastperf',classname='full'),
+    	FieldPanel('modeling_simulation_pastperf',classname='full'),
+    	FieldPanel('intel_pastperf',classname='full'),
+    	FieldPanel('cybersecurity_pastperf',classname='full'),
+    	FieldPanel('systems_analysis_eng_pastperf',classname='full'),
+    	FieldPanel('assistive_tech_pastperf',classname='full'),
+    	FieldPanel('document_management_pastperf',classname='full'),
+    	FieldPanel('distance_learning_pastperf',classname='full'),
+    	FieldPanel('technical_admin_pastperf',classname='full'),    	    	    	    	
     ]
 
 
@@ -119,39 +133,116 @@ class CareersPage(Page):
     benefits_retirement_details = RichTextField(blank=True)                                    
 
     content_panels = Page.content_panels + [
-    	FieldPanel('join_us_blurb',classname="full"),
-    	FieldPanel('benefits_holidays_details',classname="full"),
-    	FieldPanel('benefits_holidaypay_details',classname="full"),
-    	FieldPanel('benefits_pto_details',classname="full"),
-    	FieldPanel('benefits_health_insurance_details',classname="full"),
-    	FieldPanel('benefits_disability_details',classname="full"),
-    	FieldPanel('benefits_retirement_details',classname="full"),
+    	FieldPanel('join_us_blurb',classname='full'),
+    	FieldPanel('benefits_holidays_details',classname='full'),
+    	FieldPanel('benefits_holidaypay_details',classname='full'),
+    	FieldPanel('benefits_pto_details',classname='full'),
+    	FieldPanel('benefits_health_insurance_details',classname='full'),
+    	FieldPanel('benefits_disability_details',classname='full'),
+    	FieldPanel('benefits_retirement_details',classname='full'),
     ]
 
 
 class ContactPage(Page):
-    bd_contacts = RichTextField(blank=True)
-    contractvehicles_contacts = RichTextField(blank=True)
-    hr_contacts = RichTextField(blank=True)
-    general_contacts = RichTextField(blank=True)                                   
-
     content_panels = Page.content_panels + [
-    	FieldPanel('bd_contacts',classname="full"),
-    	FieldPanel('contractvehicles_contacts',classname="full"),
-    	FieldPanel('hr_contacts',classname="full"),
-    	FieldPanel('general_contacts',classname="full"),
+        InlinePanel('bd_contacts',label='BD Contacts'),
+        InlinePanel('hr_contacts',label='HR Contacts'),
+        InlinePanel('contract_vehicle_contacts',label='Contract Vehicles Contacts'),
+    	InlinePanel('general_contacts',label='General Contacts'),
     ]
 
 
-class ContactPage(Page):
-    bd_contacts = RichTextField(blank=True)
-    contractvehicles_contacts = RichTextField(blank=True)
-    hr_contacts = RichTextField(blank=True)
-    general_contacts = RichTextField(blank=True)                                   
+class ContactPageBdContacts(Orderable):
+    page = ParentalKey(ContactPage, on_delete=models.CASCADE, related_name='bd_contacts')
+    name = models.CharField(blank=True,max_length=250)
+    title = models.CharField(blank=True,max_length=250)
+    email = models.CharField(blank=True,max_length=250)
+    phone = models.CharField(blank=True,max_length=250)
+    fax = models.CharField(blank=True,max_length=250)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('title'),
+        FieldPanel('email'),
+        FieldPanel('phone'),
+        FieldPanel('fax'),
+    ]
+
+
+class ContactPageHrContacts(Orderable):
+    page = ParentalKey(ContactPage, on_delete=models.CASCADE, related_name='hr_contacts')
+    name = models.CharField(blank=True,max_length=250)
+    title = models.CharField(blank=True,max_length=250)
+    email = models.CharField(blank=True,max_length=250)
+    phone = models.CharField(blank=True,max_length=250)
+    fax = models.CharField(blank=True,max_length=250)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('title'),
+        FieldPanel('email'),
+        FieldPanel('phone'),
+        FieldPanel('fax'),
+    ]
+
+
+class ContactPageContractVehicleContacts(Orderable):
+    page = ParentalKey(ContactPage, on_delete=models.CASCADE, related_name='contract_vehicle_contacts')
+    name = models.CharField(blank=True,max_length=250)
+    title = models.CharField(blank=True,max_length=250)
+    email = models.CharField(blank=True,max_length=250)
+    phone = models.CharField(blank=True,max_length=250)
+    fax = models.CharField(blank=True,max_length=250)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('title'),
+        FieldPanel('email'),
+        FieldPanel('phone'),
+        FieldPanel('fax'),
+    ]
+
+
+class ContactPageGeneralContacts(Orderable):
+    page = ParentalKey(ContactPage, on_delete=models.CASCADE, related_name='general_contacts')
+    name = models.CharField(blank=True,max_length=250)
+    title = models.CharField(blank=True,max_length=250)
+    email = models.CharField(blank=True,max_length=250)
+    phone = models.CharField(blank=True,max_length=250)
+    fax = models.CharField(blank=True,max_length=250)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('title'),
+        FieldPanel('email'),
+        FieldPanel('phone'),
+        FieldPanel('fax'),
+    ]    
+
+
+class CyberincubatorPage(Page):
+    cyber_incubator_blurb = RichTextField(blank=True)
+    program_overview = RichTextField(blank=True)
+    participant_expectations = RichTextField(blank=True)
+    program_benefits = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('bd_contacts',classname="full"),
-        FieldPanel('contractvehicles_contacts',classname="full"),
-        FieldPanel('hr_contacts',classname="full"),
-        FieldPanel('general_contacts',classname="full"),
+        FieldPanel('cyber_incubator_blurb',classname='full'),
+        FieldPanel('program_overview',classname='full'),
+        FieldPanel('participant_expectations',classname='full'),
+        FieldPanel('program_benefits',classname='full'),
+        InlinePanel('cyberincubator_internship_descriptions',label='Internship Descriptions')
+    ]
+
+
+class CyberincubatorPageInternshipDescriptions(Orderable):
+    page = ParentalKey(CyberincubatorPage, on_delete=models.CASCADE, related_name='cyberincubator_internship_descriptions')
+    header = models.CharField(blank=True, max_length=250)
+    description = models.TextField(blank=True, max_length=500)
+    icon_code = models.CharField(blank=True, max_length=16)
+
+    panels = [
+        FieldPanel('header'),
+        FieldPanel('icon_code'),
+        FieldPanel('description'),
     ]
